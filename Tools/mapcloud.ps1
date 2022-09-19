c7df6d94021acb20f262cc26a21391c7ef04b2b2
Andrii Pylypenko <andrii.pylypenko@gws.ms>
2022-09-19 06:43:35 +0200

    upd

---
+++ Tools/mapcloud.ps1
@@ -0,0 +1,3 @@
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDMock" -Target "NDMock"
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDMockExamples" -Target "NDMockExamples"
+New-Item -ItemType SymbolicLink -Path "k:\AosService\PackagesLocalDirectory\NDMockTests" -Target "NDMockTests"
---
+++ Tools/unmapcloud.ps1
@@ -0,0 +1,3 @@
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDMock").Delete()
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDMockExamples").Delete()
+(Get-Item "k:\AosService\PackagesLocalDirectory\NDMockTests").Delete()
